b45158dbbe331a3a03978c0c8fe51f618f4e6e42
karan-seangar <seangarkaran@gmail.com>
2025-01-18 15:49:11 +0530

    primenumber file added

---
+++ prime.java
@@ -0,0 +1,17 @@
+public class prime {
+    public static void main(String[] args) {
+        System.out.println("Prime numbers between 1 and 10:");
+        for (int num = 2; num <= 10; num++) {
+            boolean isPrime = true;
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
